0d7cce4dea796d46fcc7bd08e048fe127a5ee64f
Prarthit <prarthitmehra@gmail.com>
2020-05-07 11:59:35 +0530

    Set no of questions functionality added

--- js/app.js
+++ js/app.js
@@ -11,24 +11,53 @@ else {
 }
 
 function jsload() {
+    let numQuesInput = document.querySelector('input[type=number]');
+
+    // Initially add 50 questions
+    addRemoveQues(numQuesInput.value);
+
+    // On click of download button
+    document.querySelector("button").onclick = btnclick;
+
+    // Enter keypress on input field or out of focus, add/remove ques
+    numQuesInput.onkeypress = function (e) {
+        if (!e) e = window.event;
+        var keyCode = e.keyCode || e.which;
+        if (keyCode == '13') {
+            addRemoveQues(e.target.value);
+        }
+    }
+    numQuesInput.onblur = (e) => addRemoveQues(e.target.value);
+}
+
+function addRemoveQues(numQues) {
+    if (numQues < 0 || numQues > 1000)
+        return;
+
     let table = document.querySelector('table'),
         trow = document.querySelector('tr');
 
-    for (let i = 1; i <= 50; i++) {
-        let inputs = trow.querySelectorAll('input');
-        let td = trow.querySelector('td');
+    // table.childcount - 1 because there is tbody child in table which we don't want to count
+    if (numQues > table.childElementCount - 1) {
+        for (let i = table.childElementCount; i <= numQues; i++) {
+            trow = trow.cloneNode(true);
+            let inputs = trow.querySelectorAll('input');
+            let td = trow.querySelector('td');
 
-        td.innerHTML = i + '.';
-        inputs.forEach(element => {
-            element.name = i;
-            element.onclick = toggleRadio;
-        });
+            td.innerHTML = i + '.';
+            inputs.forEach(element => {
+                element.name = i;
+                element.onclick = toggleRadio;
+            });
 
-        table.appendChild(trow);
-        trow = trow.cloneNode(true);
+            table.appendChild(trow);
+        }
     }
-
-    document.querySelector("button").onclick = btnclick;
+    else {
+        for (let i = table.children.length - 1; i > numQues; i--) {
+            table.removeChild(table.lastElementChild);
+        }
+    }
 }
 
 function toggleRadio() {
